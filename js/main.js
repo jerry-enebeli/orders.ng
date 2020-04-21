0e018af17c8b309984808e197feca39825939a71
jerry <jerryenebeli@icloud.com>
2020-04-21 22:00:52 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const businessName = urlParams.get("business");
 const userId = urlParams.get("id");
 const linkId = urlParams.get("link");
 
-let productName
-let price
+let productName;
+let priceNum
+let price;
 
 firebase.initializeApp({
   apiKey: "AIzaSyD-mNaskMlrezICXddeo6KED1gemyLLzc8",
@@ -17,18 +18,37 @@ firebase.initializeApp({
   appId: "1:1080782001747:web:ae3c53704a68fde2f758a6",
   measurementId: "G-EDCQCN0F56",
 });
-
+var db = firebase.firestore();
 const getOrders = async () => {
-  var db = firebase.firestore();
   const result = await db.collection("Links").doc(linkId).get();
-  productName = result.data().productName
-  price =  '₦' + new Intl.NumberFormat("en-US", {}).format(result.data().price);
+  productName = result.data().productName;
+  price = "₦" + new Intl.NumberFormat("en-US", {}).format(result.data().price);
+  priceNum = result.data().price;
 
-
-document.getElementById("product-name").innerText = productName;
-document.getElementById("product-price").innerText = price;
+  document.getElementById("product-name").innerText = productName;
+  document.getElementById("product-price").innerText = price;
 };
 
+const placeOrder = async (e) => {
+  const customer_name = document.getElementById("customer_name").value;
+  const customer_email = document.getElementById("customer_email").value;
+  const customer_phone = document.getElementById("customer_phone").value;
+  const delivery_address = document.getElementById("delivery_address").value;
+  const qty = document.getElementById("qty").value;
+  console.log(price, qty)
+  const data = {
+    customer_email,
+    customer_name,
+    customer_phone,
+    delivery_address,
+    userId,
+    qty: qty || 1,
+    total_amount: parseInt(priceNum) * parseInt(qty),
+    status: "pending",
+    date: new Date().getTime(),
+  };
+  await db.collection("Orders").add(data);
+};
 getOrders();
 
 document.getElementById("business-name").innerText = businessName;
